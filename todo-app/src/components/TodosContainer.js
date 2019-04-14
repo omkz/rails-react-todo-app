1f65d48497c6cac24d7af175849fee385fa83a95
Kurnia Muhamad <omenkzz@gmail.com>
2019-04-14 20:25:59 +0700

    Create Todo feature

--- todo-app/src/components/TodosContainer.js
+++ todo-app/src/components/TodosContainer.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import update from 'immutability-helper'
 
 class TodosContainer extends Component {
+ 
   constructor(props) {
     super(props)
     this.state = {
-      todos: []
+      todos: [],
+      inputValue: ''
     }
-  }
+	}
 
   getTodos() {
     axios.get('/api/v1/todos')
-    .then(response => {
-      this.setState({todos: response.data})
-    })
-    .catch(error => console.log(error))
+      .then(response => {
+        this.setState({ todos: response.data })
+      })
+      .catch(error => console.log(error))
+  }
+
+  handleChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  }
+
+  createTodo = (e) => {
+    if (e.key === 'Enter' && !(e.target.value === '')) {
+      axios.post('/api/v1/todos', {todo: {title: e.target.value}})
+      .then(response => {
+        const todos = update(this.state.todos, {
+          $splice: [[0, 0, response.data]]
+        })
+        this.setState({
+          todos: todos,
+          inputValue: ''
+        })
+      })
+      .catch(error => console.log(error))      
+    }    
   }
 
   componentDidMount() {
@@ -25,24 +48,26 @@ class TodosContainer extends Component {
     return (
       <div>
         <div className="inputContainer">
-          <input className="taskInput" type="text" 
+          <input className="taskInput" type="text"
             placeholder="Add a task" maxLength="50"
-            onKeyPress={this.createTodo} />
-        </div>  	    
-	<div className="listWrapper">
-	   <ul className="taskList">
-		  {this.state.todos.map((todo) => {
-		    return(
-		      <li className="task" todo={todo} key={todo.id}>
-			<input className="taskCheckbox" type="checkbox" />              
-			<label className="taskLabel">{todo.title}</label>
-			<span className="deleteTaskBtn">x</span>
-		      </li>
-		    )       
-		  })} 	    
-	   </ul>
-	</div>
-     </div>
+            onKeyPress={this.createTodo}
+            value={this.state.inputValue} onChange={this.handleChange} />
+
+        </div>
+        <div className="listWrapper">
+          <ul className="taskList">
+            {this.state.todos.map((todo) => {
+              return (
+                <li className="task" todo={todo} key={todo.id}>
+                  <input className="taskCheckbox" type="checkbox" />
+                  <label className="taskLabel">{todo.title}</label>
+                  <span className="deleteTaskBtn">x</span>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
     )
   }
 }
